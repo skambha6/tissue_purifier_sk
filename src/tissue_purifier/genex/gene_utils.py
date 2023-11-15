7cf45fd74b58e651dbe06ea8ae55088d9391c2e3
skambha6 <skambha6@jhu.edu>
2023-11-15 19:05:29 +0000

    added scaling variables to 'un-regularize' certain coefficients

--- src/tissue_purifier/genex/gene_utils.py
+++ src/tissue_purifier/genex/gene_utils.py
@@ -1,7 +1,9 @@
 import torch
 import numpy
+import numpy as np
 from scanpy import AnnData
 from scanpy import pp
+import pandas as pd
 from typing import NamedTuple, Union, List, Any, Optional
 from sklearn.model_selection import StratifiedShuffleSplit, ShuffleSplit
 from tissue_purifier.utils.validation_util import SmartPca
@@ -22,6 +24,51 @@ def _make_labels(y: Union[torch.Tensor, numpy.ndarray, List[Any]]) -> (torch.Ten
     labels = torch.tensor([y_to_ids[tmp] for tmp in y_np])
     return labels, y_to_ids
 
+def filter_anndata(anndata: AnnData,
+        fc_bc_min_umi: int = 500,                  # filter cells with too few UMI
+        fc_bc_max_umi: int = 3000,                 # filter cells with too many UMI
+        fc_bc_min_n_genes_by_counts: int = 10,     # filter cells with too few GENES
+        fc_bc_max_n_genes_by_counts: int = 2500,   # filter cells with too many GENES
+        fc_bc_max_pct_counts_mt: int = 5,          # filter cells with mitocrondial fraction too high
+        fctype_bc_min_cells_absolute = 100,        # filter cell-types which are too RARE in absolute number
+        fctype_bc_min_cells_frequency = 0.01,      # filter cell-types which are too RARE in relative abundance
+        fg_bc_min_pct_cells_by_counts: int = 10,   # filter genes which appear in too few percentage of CELLS
+        fg_bc_high_var: int = 1000,                # filter genes to top n highly variable genes
+        cell_type_key: str = None):      
+
+        ## add metrics
+        anndata.var['mt'] = anndata.var_names.str.startswith('mt-')  # annotate the group of mitochondrial genes as 'mt'
+        pp.calculate_qc_metrics(anndata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
+        
+        anndata.var["pct_cells_by_counts"] = anndata.var["n_cells_by_counts"]/anndata.X.shape[0] * 100
+        
+        ## determine cell types to keep
+        counts = anndata.obs[cell_type_key].value_counts()
+        freqs = anndata.obs[cell_type_key].value_counts(normalize=True)
+        mask1 = (counts > fctype_bc_min_cells_absolute)
+        mask2 = (freqs > fctype_bc_min_cells_frequency)
+        mask = mask1 * mask2
+        cell_type_keep = mask.index[mask]
+        anndata.obs["keep_ctype"] = anndata.obs[cell_type_key].apply(lambda x: x in cell_type_keep)
+        
+        ## filter anndata according to criteria
+        anndata = anndata[anndata.obs["total_counts"] > fc_bc_min_umi, :] 
+        anndata = anndata[anndata.obs["total_counts"] < fc_bc_max_umi, :] 
+        anndata = anndata[anndata.obs["n_genes_by_counts"] > fc_bc_min_n_genes_by_counts, :] 
+        anndata = anndata[anndata.obs["n_genes_by_counts"] < fc_bc_max_n_genes_by_counts, :] 
+        anndata = anndata[anndata.obs["pct_counts_mt"] < fc_bc_max_pct_counts_mt, :]
+        anndata = anndata[anndata.obs["keep_ctype"] == True, :]
+        anndata = anndata[:, anndata.var["pct_cells_by_counts"] > fg_bc_min_pct_cells_by_counts]
+        
+        ## filter to high var genes
+        anndata_norm = pp.normalize_total(anndata, copy=True)
+        anndata_log = pp.log1p(anndata_norm, copy=True)
+        pp.highly_variable_genes(anndata_log, n_top_genes=fg_bc_high_var)
+        anndata = anndata[:,anndata_log.var['highly_variable']]
+        
+        
+        return anndata
+
 
 class GeneDataset(NamedTuple):
     """
@@ -40,9 +87,6 @@ class GeneDataset(NamedTuple):
 
     #: long tensor with the count data of shape (n, g)
     counts: torch.Tensor
-    
-    #: float tensor with the cell type proportions for each pixel of shape (n, k)
-    cell_type_props: torch.Tensor
 
     #: number of cell types
     k_cell_types: int
@@ -52,8 +96,6 @@ class GeneDataset(NamedTuple):
 
     #: list of the gene names
     gene_names: List[str]
-    
-    
     
 
     def describe(self):
@@ -65,7 +107,6 @@ class GeneDataset(NamedTuple):
                 print("{} ---> list of length {}".format(k.ljust(20), len(v)))
             else:
                 print("{} ---> {}".format(k.ljust(20), v.shape))
-
 
 def make_gene_dataset_from_anndata(
         anndata: AnnData,
@@ -117,13 +158,15 @@ def make_gene_dataset_from_anndata(
 
     cell_types = list(anndata.obs[cell_type_key].values)
     cell_type_ids_n, mapping_dict = _make_labels(cell_types)
-    counts_ng = torch.tensor(anndata.X.toarray()).long()
+    ## TODO: change back to long if poisson glm is finalized
+    # counts_ng = torch.tensor(anndata.X.toarray()).long()
+    counts_ng = torch.tensor(anndata.X.toarray()).float()
     covariates_nl_raw = torch.tensor(anndata.obsm[covariate_key])
     
 
     ## TODO: check this
     ## use mapping_dict.keys() so that order of cell types matches
-    cell_type_props = torch.tensor(anndata.obs[mapping_dict.keys()].to_numpy()).float()
+    cell_type_props = torch.tensor(anndata.obsm[cell_type_prop_key].to_numpy()).float()
 
 
     if not torch.all(torch.isfinite(covariates_nl_raw)):
@@ -159,7 +202,6 @@ def make_gene_dataset_from_anndata(
         cell_type_props=_to_torch(cell_type_props),
         covariates=_to_torch(new_covariate),
         counts=_to_torch(counts_ng),
-        cell_type_props = _to_torch(cell_type_props),
         k_cell_types=k_cell_types,
         cell_type_mapping=mapping_dict,
         gene_names=list(anndata.var_names),
@@ -183,7 +225,7 @@ def train_test_val_split(
         val_size: the relative size of the val dataset
         n_splits: how many times to split the data
         random_state: specify the random state for reproducibility
-        stratify: If true the tran/test/val are stratified so that they contain approximately the same
+        stratify: If true the train/test/val are stratified so that they contain approximately the same
             number of example from each class. If data is a list of arrays the 2nd array is assumed to represent the
             class. If data is a GeneDataset the class is the cell_type.
 
@@ -252,6 +294,108 @@ def train_test_val_split(
         sss1 = ShuffleSplit(n_splits=1,
                             train_size=val_size_norm1,
                             test_size=test_size_norm1,
+                            random_state=random_state)
+
+    # Part common to both stratified and not stratified
+    trains, tests, vals = None, None, None
+    for index_train, index_test_and_val in sss0.split(*arrays[:2]):
+        trains = [a[index_train] for a in arrays]
+        test_and_val = [a[index_test_and_val] for a in arrays]
+        for index_val, index_test in sss1.split(*test_and_val[:2]):
+            tests = [a[index_test] for a in test_and_val]
+            vals = [a[index_val] for a in test_and_val]
+
+        if isinstance(data, GeneDataset):
+            # copy the k_cell_types and cell_type_mapping in the train/test/val
+            trains += [data.k_cell_types, data.cell_type_mapping, data.gene_names]
+            tests += [data.k_cell_types, data.cell_type_mapping, data.gene_names]
+            vals += [data.k_cell_types, data.cell_type_mapping, data.gene_names]
+            yield GeneDataset._make(trains), GeneDataset._make(tests), GeneDataset._make(vals)
+        else:
+            yield trains, tests, vals
+            
+            
+def train_test_split(
+        data: Union[List[torch.Tensor], List[numpy.ndarray], GeneDataset],
+        train_size: float = 0.8,
+        test_size: float = 0.2,
+        n_splits: int = 1,
+        random_state: int = None,
+        stratify: bool = True,
+        spatial: bool = False):
+    """
+    Utility function used to split the data into train/test/val.
+
+    Args:
+        data: the data to split into train/test/val
+        train_size: the relative size of the train dataset
+        test_size: the relative size of the test dataset
+        val_size: the relative size of the val dataset
+        n_splits: how many times to split the data
+        random_state: specify the random state for reproducibility
+        stratify: If true the train/test are stratified so that they contain approximately the same
+            number of example from each class. If data is a list of arrays the 2nd array is assumed to represent the
+            class. If data is a GeneDataset the class is the cell_type.
+        spatial: If true, the train/test are stratified based on spatial coordinates of patches. If both spatial and stratify are true,
+            a spatial split that best preserves stratification will be found.
+
+    Returns:
+        tuple: yields multiple splits of the data.
+
+    Example:
+          >>> for train, test, val in train_test_val_split(data=[x,y,z]):
+          >>>       x_train, y_train, z_train = train
+          >>>       x_test, y_test, z_test = test
+          >>>       x_val, y_val, z_val = val
+          >>>       ... do something ...
+
+    Example:
+          >>> for train, test, val in train_test_val_split(data=GeneDataset):
+          >>>       assert isinstance(train, GeneDataset)
+          >>>       assert isinstance(test, GeneDataset)
+          >>>       assert isinstance(val, GeneDataset)
+          >>>       ... do something ...
+    """
+
+    if train_size <= 0:
+        raise ValueError("Train_size must be > 0")
+    if test_size <= 0:
+        raise ValueError("Test_size must be > 0")
+    if val_size <= 0:
+        raise ValueError("Val_size must be > 0")
+
+    if isinstance(data, List):
+        arrays = data
+    elif isinstance(data, GeneDataset):
+        # same order as in the definition of GeneDataset NamedTuple
+        ## TODO: check order here
+        arrays = [data.covariates, data.cell_type_ids, data.cell_type_props, data.counts]
+    else:
+        raise ValueError("data must be a list or a GeneDataset")
+
+    dims_actual = [a.shape[0] for a in arrays]
+    dims_expected = [dims_actual[0]] * len(dims_actual)
+    assert all(a == b for a, b in zip(dims_actual, dims_expected)), \
+        "Error. All leading dimensions should be the same"
+
+    # Normalize the train/test/val sizes
+    norm0 = train_size + test_size + val_size
+    train_size_norm0 = train_size / norm0
+    test_and_val_size_norm0 = (test_size + val_size) / norm0
+
+    norm1 = test_size + val_size
+    test_size_norm1 = test_size / norm1
+    val_size_norm1 = val_size / norm1
+
+    if stratify:
+        sss0 = StratifiedShuffleSplit(n_splits=n_splits,
+                                      train_size=train_size_norm0,
+                                      test_size=test_and_val_size_norm0,
+                                      random_state=random_state)
+    else:
+        sss0 = ShuffleSplit(n_splits=n_splits,
+                            train_size=train_size_norm0,
+                            test_size=test_and_val_size_norm0,
                             random_state=random_state)
 
     # Part common to both stratified and not stratified
--- src/tissue_purifier/genex/poisson_glm.py
+++ src/tissue_purifier/genex/poisson_glm.py
@@ -5,11 +5,12 @@ import numpy as np
 import time
 import pandas as pd
 import matplotlib.pyplot as plt
-from tissue_purifier.genex.gene_utils import GeneDataset # relative vs absolute imports 
+from tissue_purifier.genex.gene_utils import GeneDataset # relat0ive vs absolute imports 
 
 
 from sklearn.linear_model import PoissonRegressor
 from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import StandardScaler
 
 
 class GeneRegression:
@@ -18,11 +19,16 @@ class GeneRegression:
     The counts are modelled as a Poisson process. See documentation for more details.
     """
     
-    def __init__(self, use_covariates):
+    def __init__(self, use_covariates, umi_scaling, cell_type_prop_scaling, scale_covariates: bool = False):
         self._train_kargs = None
         self.clf_g = {}
         self._use_covariates = use_covariates
+        self._scale_covariates = scale_covariates
         self._alpha_dict = {}
+        self._umi_scaling = umi_scaling
+        self._cell_type_prop_scaling = cell_type_prop_scaling
+        
+        ## assert umi scaling and cell type prop scaling are integers > 0
         
     def _get_regression_X(self, dataset) -> np.array:
         
@@ -30,9 +36,9 @@ class GeneRegression:
         cell_type_ids = dataset.cell_type_ids.long()
         total_umi_n = counts_ng.sum(dim=-1)
         covariates_nl = dataset.covariates.float().cpu()
-        cell_type_props_nk = dataset.cell_type_props
+        cell_type_props_nk = self._cell_type_prop_scaling*dataset.cell_type_props
         
-        log_total_umi_n1 = np.log(total_umi_n).reshape(-1,1)
+        log_total_umi_n1 = self._umi_scaling*np.log(total_umi_n).reshape(-1,1)
         
         if self._use_covariates:
             X = np.concatenate([log_total_umi_n1, 
@@ -41,6 +47,10 @@ class GeneRegression:
         else:
              X = np.concatenate([log_total_umi_n1, 
                             cell_type_props_nk], axis=1)
+            
+        if self._scale_covariates:
+            scaler = StandardScaler()
+            X = scaler.fit_transform(X)
             
         return X
     
@@ -82,16 +92,37 @@ class GeneRegression:
         gene_names_kg = numpy.array(gene_names_list)[gene_codes.cpu().numpy()]
         return gene_names_kg
     
+    def get_alpha_dict(self) -> dict:
+        """ Return alpha regularization dictionary with per_gene alpha """
+        return self._alpha_dict
+    
+    def set_alpha_dict(self, alpha_dict: dict):
+        """ Set alpha regularization dictionary with per_gene alpha """
+        self._alpha_dict = alpha_dict
+    
     def train(self,
               train_dataset: GeneDataset,
+              val_dataset: GeneDataset = None,
               n_steps: int = 100,
               print_frequency: int = 100,
               use_covariates: bool = True,
               regularization_sweep: bool = False,
-              alpha_regularization_strengths: np.array = np.array([1.0]),
+              fit_intercept: bool = False,
+              alpha_regularization_strengths: np.array = np.array([0.01]),
+              alpha_dict: dict = None,
               metric: str = None, ## return dsquared by default 
               **kargs
               ):
+        
+        ## Create fit_alpha_dict flag based on whether alpha_dict is passed. If alpha_dict is passed, set fit_alpha_dict flag to False
+        ## If fit_alpha_dict flag is false, fit_model() will not over-write alpha_dict
+        fit_alpha_dict = True
+        if alpha_dict is not None:
+            print("Using user-supplied alpha regularization dictionary")
+            fit_alpha_dict = False
+            
+        if use_covariates:
+            print("Using covariates")
             
         # prepare train kargs dict
         train_kargs = {
@@ -101,36 +132,50 @@ class GeneRegression:
             'alpha_regularization_strengths': alpha_regularization_strengths,
             'cell_type_mapping': train_dataset.cell_type_mapping,
             'gene_names': train_dataset.gene_names,
+            'fit_alpha_dict': fit_alpha_dict
         }
         
         # make a copy so that can edit train_kargs without changing _train_kargs
         self._train_kargs = train_kargs.copy()
 
         # Unpack the dataset
-        counts_ng = train_dataset.counts.long()
+        counts_ng_train = train_dataset.counts.long()
         cell_type_ids = train_dataset.cell_type_ids.long()
-        total_umi_n = counts_ng.sum(dim=-1)
+        total_umi_n = counts_ng_train.sum(dim=-1)
+        
+        # Prepare validation counts if regularization_sweep is true
+        if regularization_sweep:
+            assert val_dataset is not None
+            counts_ng_val = val_dataset.counts.long()
+        else:
+            counts_ng_val = None
         
         # Prepare arguments for training
-        train_kargs["n_cells"] = counts_ng.shape[0]
-        train_kargs["g_genes"] = counts_ng.shape[1]
+        train_kargs["n_cells"] = counts_ng_train.shape[0]
+        train_kargs["g_genes"] = counts_ng_train.shape[1]
         train_kargs["l_cov"] = train_dataset.covariates.shape[-1]
         train_kargs["k_cell_types"] = train_dataset.k_cell_types
-        train_kargs["counts_ng"] = counts_ng.cpu()
+        train_kargs["counts_ng"] = counts_ng_train.cpu()
         train_kargs["total_umi_n"] = total_umi_n.cpu()
         train_kargs["covariates_nl"] = train_dataset.covariates.float().cpu()
         train_kargs["cell_type_ids_n"] = cell_type_ids
         train_kargs["cell_type_props"] = train_dataset.cell_type_props 
+        train_kargs["alpha_dict"] = alpha_dict
         
         self._train_kargs = train_kargs
         
-        self.fit_model(train_dataset, counts_ng, print_frequency, train_kargs)
-    
+        # fit model
+        self.fit_model(train_dataset, val_dataset, counts_ng_train, counts_ng_val, fit_intercept, print_frequency, train_kargs)
+
     def fit_model(self, train_dataset: GeneDataset, 
-                        counts_ng: np.array,
+                        val_dataset: GeneDataset,
+                        counts_ng_train: np.array,
+                        counts_ng_val: np.array,
+                        fit_intercept: bool,
                         print_frequency: int,
                         kargs: dict):
         
+        ## prepare coefficients for regression
         total_umi_n = kargs["total_umi_n"]
         cell_type_props_nk = kargs['cell_type_props']
         covariates_nl = kargs['covariates_nl']
@@ -140,66 +185,104 @@ class GeneRegression:
         gene_names = kargs['gene_names']
         regularization_sweep = kargs['regularization_sweep']
         alpha_regularization_strengths = kargs['alpha_regularization_strengths']
+        fit_alpha_dict = kargs["fit_alpha_dict"]
+        alpha_dict = kargs["alpha_dict"]
         
+        ## get coefficients for regression
         
+        X_train = self._get_regression_X(train_dataset)
         
-        log_total_umi_n1 = np.log(np.asarray(total_umi_n)).reshape(-1,1)
-        
-        X = self._get_regression_X(train_dataset)
-        
-        # ## add log umi information as a covariate
-        # if self._use_covariates:
-        #     X = np.concatenate([log_total_umi_n1, 
-        #                     cell_type_props_nk], axis=1)
-        # else:
-        #     X = np.concatenate([log_total_umi_n1, 
-        #                         cell_type_props_nk,
-        #                         covariates_nl], axis=1) 
+        if regularization_sweep:
+            X_val = self._get_regression_X(val_dataset)
+    
+        if not regularization_sweep:
+            if fit_alpha_dict:
+                 ## TODO: assert that alpha regularization is a numpy array
+                assert alpha_regularization_strengths.size == 1
+                for gene_name in gene_names:
+                    self._alpha_dict[gene_name] = alpha_regularization_strengths[0]
+            else:
+                assert alpha_dict is not None
+                ## TODO: assert all genes are present in user given alpha_dict
+                self._alpha_dict = alpha_dict
         
         ## Train a separate GLM for each gene
-        for i in range(g_genes):
-
-            if i % print_frequency == 0:
-                print('finished ' + str(i) + ' genes')
-                
-            gene_name = gene_names[i]
-            y = np.ravel(counts_ng[:,i])
-
-            if not regularization_sweep:
-                ## output warning message
-                assert len(alpha_regularization_strengths) == 1
-                self.clf_g[gene_name] = PoissonRegressor(alpha = alpha_regularization_strengths[0], fit_intercept=False, max_iter=n_steps).fit(X, y)
-            else:
-                best_estimator, best_params, cv_results = self.alpha_cross_validation(n_steps, alpha_regularization_strengths, X, y)
-                
-                self._alpha_dict[gene_name] = best_params['alpha']
-                
-                self.clf_g[gene_name] = best_estimator
+    
+        print('start fitting genes')
+        for g_ind in range(g_genes):
             
-    def alpha_cross_validation(self, n_steps, alpha_regularization_strengths, X, y):
+            
+            import time
+            
+            ## output progress message
+            if (g_ind+1) % print_frequency == 0:
+                print('finished ' + str(g_ind+1) + ' genes')
+                
+            # get gene name
+            gene_name = gene_names[g_ind]
+            
+            # get gene counts for fitting model
+            y_train = np.ravel(counts_ng_train[:,g_ind])
+            
+            # get gene counts for choosing alpha regularization value
+            if regularization_sweep:
+                y_val = np.ravel(counts_ng_val[:,g_ind])
+                val_scores = self.regularization_validation(X_train, y_train, X_val, y_val, alpha_regularization_strengths, n_steps)
+                ## choose alpha value based on validation set performance
+                self._alpha_dict[gene_name] = alpha_regularization_strengths[np.argmax(val_scores)]
+                
+            # performance profiling
+            # start_time = time.time()
+            ## fit GLM 
+            self.clf_g[gene_name] = PoissonRegressor(alpha = self._alpha_dict[gene_name], fit_intercept=fit_intercept, solver='newton-cholesky', max_iter=n_steps).fit(X_train, y_train)
+            # end_time = time.time()
+            # print(str(end_time-start_time) + ' seconds elapsed per gene')
+            
+                
+        ## output median alpha if regularization sweep was run
+        if regularization_sweep:
+            print("median alpha: " + str(np.median(list(self._alpha_dict.values()))))
+            
+#     def alpha_cross_validation(self, n_steps, alpha_regularization_strengths, X, y):
         
-        ## add in support for user-defined scoring function
+#         ## add in support for user-defined scoring function
         
-        param_dict = {'alpha': alpha_regularization_strengths}
-        grid_search = GridSearchCV(PoissonRegressor(fit_intercept=False, max_iter=n_steps), param_dict)
-        grid_search.fit(X,y)
+#         param_dict = {'alpha': alpha_regularization_strengths}
+#         grid_search = GridSearchCV(PoissonRegressor(fit_intercept=False, max_iter=n_steps), param_dict)
+#         grid_search.fit(X,y)
         
-        cv_results = grid_search.cv_results_
-        best_estimator = grid_search.best_estimator_
-        best_params = grid_search.best_params_
+#         cv_results = grid_search.cv_results_
+#         best_estimator = grid_search.best_estimator_
+#         best_params = grid_search.best_params_
         
-        return best_estimator, best_params, cv_results
+#         return best_estimator, best_params, cv_results
+    
+    
+    def regularization_validation(self, X_train, y_train, X_val, y_val, alphas, n_steps):
+        
+        ## Train separate GLM for each alpha and output corresponding d_sq scores on validation set
+        val_scores = []
+
+        for alpha in alphas:
+            estimator = PoissonRegressor(alpha=alpha, fit_intercept=False, solver='newton-cholesky', max_iter=n_steps)
+            estimator.fit(X_train,y_train)
+            val_score = estimator.score(X_val, y_val)
+            
+            val_scores.append(val_score)
+
+        return val_scores
         
             
     def predict(self,
             dataset: GeneDataset,
             return_metrics: bool = False) -> (pd.DataFrame, pd.DataFrame):
         
+        # constants
         n, g = dataset.counts.shape[:2]
         n, l = dataset.covariates.shape[:2]
         k = dataset.k_cell_types
 
-         # dataset
+        # dataset
         counts_ng = np.array(dataset.counts.long().cpu())
         cell_type_ids = dataset.cell_type_ids.long().cpu()
         covariates_nl1 = dataset.covariates.unsqueeze(dim=-1).float().cpu()
@@ -266,6 +349,9 @@ class GeneRegression:
         # df_counts_ng["cell_type"] = cell_names_ng.flatten()
         # df_counts_ng["gene"] = gene_names_ng.flatten()
         
+        df_d_sq_g = pd.DataFrame(d_sq_g, columns=["D_sq"])
+        df_d_sq_g.index = self._get_gene_list()
+        
         if gr_baseline is not None:
             
             q_baseline_mu_kg = np.zeros((unique_cell_types.shape[0], g))
@@ -277,7 +363,7 @@ class GeneRegression:
                 mask = (cell_type_ids == cell_type)
                 q_tg_baseline = q_ng_baseline[mask]
                 ## TODO: make size a user flag
-                sample_ind = np.random.choice(q_tg_baseline.shape[0], size=500)
+                sample_ind = np.random.choice(q_tg_baseline.shape[0], size=10000)
                 q_tg_baseline_sample = q_tg_baseline[sample_ind]
 
                 q_baseline_mu_kg[k] = q_tg_baseline_sample.mean(axis=0)
@@ -286,9 +372,9 @@ class GeneRegression:
                 
             q_z_kg = (q_kg - q_baseline_mu_kg)/q_baseline_std_kg
             
-            return df_metric_kg, d_sq_g, q_z_kg
+            return df_metric_kg, df_d_sq_g, q_z_kg
         else:
-            return df_metric_kg, d_sq_g
+            return df_metric_kg, df_d_sq_g, q_kg
             
         
         
