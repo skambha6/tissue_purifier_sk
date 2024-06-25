3d49ed2707310f0708beaf3d29ae61725cbb46cd
Sandeep <skambha6@jhu.edu>
2024-06-25 19:16:23 -0400

    add documentation to embedding_utils, regressor enrichment

--- src/tissue_purifier/utils/embedding_util.py
+++ src/tissue_purifier/utils/embedding_util.py
@@ -10,6 +10,8 @@ from sklearn.linear_model import RidgeClassifierCV, RidgeCV
 from tissue_purifier.utils import *
 from tissue_purifier.plots import *
 
+from tissue_purifier.data import SparseImage
+
 from tissue_purifier.utils.anndata_util import *
 
 # Set of functions to perform common desired tasks with embeddings, 
@@ -17,11 +19,25 @@ from tissue_purifier.utils.anndata_util import *
 
 def motif_query(adata_ref: anndata.AnnData,
                 adata_query: anndata.AnnData,
-                query_point: tuple,
+                query_point: Tuple[int, int],
                 rep_key: str,
                 dist_type: str = 'cosine',
                 exponentiate: bool=True ## used for cosine distance
                 ):
+    """
+    Query motif similarity between a query AnnData object at a specific point and a reference AnnData.
+
+    Parameters:
+    adata_ref (anndata.AnnData): Reference AnnData object.
+    adata_query (anndata.AnnData): Query AnnData object.
+    query_point (tuple): Coordinates (x, y) of the query point.
+    rep_key (str): Key in `.obsm` for the representation to use.
+    dist_type (str): Type of distance metric to use ('cosine' or 'euclidean'). Default is 'cosine'.
+    exponentiate (bool): Whether to exponentiate the cosine similarity. Default is True.
+
+    Returns:
+    anndata.AnnData: Updated reference AnnData object with similarity scores in `.obs['sim']`.
+    """
 
     x_query, y_query = query_point
     query_point_idx = np.argmin((adata_ref.obs['y'].values - x_query) ** 2 + (adata_ref.obs['x'].values - y_query) **2)
@@ -47,20 +63,80 @@ def motif_query(adata_ref: anndata.AnnData,
     return adata_ref
 
 
-def cluster(adata: Union[anndata.AnnData, List[anndata.AnnData]],
+def cluster(adata: anndata.AnnData,
                key: str,
                leiden_res: Union[float, list],
                n_neighbors: int = 500,
                umap_preprocess_strategy: str = 'raw',
-               cluster_patches: bool = True):
+               cluster_patches: bool = True,
+               x_key: str = 'x',
+               y_key: str = 'y',
+               category_key: str = 'cell_type_proportions'):
+    """
+    Cluster embeddings in an AnnData object by computing UMAP and running leiden clustering on the UMAP graph
+
+    Parameters:
+    adata (anndata.AnnData): Input AnnData object.
+    key (str): Key in `.obsm` for the representation to use.
+    leiden_res (Union[float, List[float]]): Resolution(s) for Leiden clustering.
+    n_neighbors (int): Number of neighbors for UMAP. Default is 500.
+    umap_preprocess_strategy (str): Preprocessing strategy for UMAP. Default is 'raw'.
+    cluster_patches (bool): If true, use coarser patch representations for clustering. If false, use spot representations "{key}_spot_features"
+    x_key (str): Key for x-coordinates in `.obs`. Default is 'x'.
+    y_key (str): Key for y-coordinates in `.obs`. Default is 'y'.
+    category_key (str): Key for category information in `.obs`. Default is 'cell_type_proportions'.
+
+    Returns:
+    anndata.AnnData: Updated AnnData object with UMAP embeddings under "umap_{key}" and clustering results under "leiden_feature_{key}_res_{res}_one_hot" in .obsm.
+    """
     
     if cluster_patches:
-        assert 'sparse_image_state_dict' in adata.uns.keys(), \
-                'Need to have sparse_image_state_dict in adata.uns to compute patch clusters'
-        pass
-        # to be implemented
+        ##TODO: support functionality for clustering a merged anndata file containing multiple samples
+        patch_properties_dict = adata.uns['sparse_image_state_dict']['patch_properties_dict']
+        print("Running UMAP")
+        smart_pca = SmartPca(preprocess_strategy='z_score')
+        smart_umap = SmartUmap(n_neighbors=n_neighbors, preprocess_strategy=umap_preprocess_strategy, n_components=2, min_dist=0.5, metric='euclidean')
+
+        input_features = patch_properties_dict[key]
+        embeddings_pca = smart_pca.fit_transform(input_features, n_components=0.9)
+        embeddings_umap = smart_umap.fit_transform(embeddings_pca)
+
+        # adata.obsm["pca_"+key] = np.array(embeddings_pca)
+        patch_properties_dict["umap_"+key] = np.array(embeddings_umap)
+        patch_properties_dict["umap_graph_"+key] = smart_umap.get_graph()
+
+        ## Compute clustering
+        print("Computing clusters")
+        umap_graph = smart_umap.get_graph()
+        smart_leiden = SmartLeiden(graph=umap_graph)
+
+        if not isinstance(leiden_res, list):
+            leiden_res = [leiden_res]
+
+        leiden_keys_one_hot = []
+        for res in leiden_res:
+            leiden_clusters = smart_leiden.cluster(resolution=res, partition_type='RBC')
+            patch_properties_dict["leiden_feature_" + key + "_" +str(res)] = leiden_clusters
+            patch_properties_dict["leiden_feature_" + key + "_res_"+str(res)+"_one_hot"] = torch.nn.functional.one_hot(torch.from_numpy(leiden_clusters).long())  
+            leiden_keys_one_hot.append("leiden_feature_" + key + "_res_"+str(res)+"_one_hot")
+
+        sp_img = SparseImage.from_anndata(adata, x_key=x_key, y_key=y_key, category_key=category_key)
+
+        for leiden_key in leiden_keys_one_hot:
+                sp_img.write_to_patch_dictionary(leiden_key, torch.Tensor(patch_properties_dict[leiden_key].float()), patches_xywh = patch_properties_dict[key + '_patch_xywh'], overwrite = True)
+
+        # Note that I use the one_hot version of the Leiden clusters to transfer the annotations
+        sp_img.transfer_patch_to_spot(
+            keys_to_transfer=leiden_keys_one_hot,
+            overwrite=True)
+
+        # print(sp_img.spot_properties_dict['leiden_feature_dino_res_0.05_one_hot'].shape)
+        out_adata = sp_img.to_anndata()
+
+        return out_adata
     else:
-        assert isinstance(adata, anndata.AnnData), 'Need to pass an anndata object to cluster'
+        assert isinstance(adata, anndata.AnnData), 'Need to pass an anndata object'
+
         ## Compute UMAP 
             
         print("Running UMAP")
@@ -90,13 +166,25 @@ def cluster(adata: Union[anndata.AnnData, List[anndata.AnnData]],
         
         return adata
 
-
 def conditional_motif_enrichment(adata: Anndata,
                                  feature_key: str,
                                  classify_or_regress: str,
                                  alpha_regularization: Union[float, list],
                                  verbose: bool = False):
-    
+    """
+    Perform conditional motif enrichment by training classifiers on spatially partitioned train/test folds.
+
+    Parameters:
+    adata (anndata.AnnData): Input AnnData object.
+    feature_key (str): Key in `.obsm` for the representation to use.
+    classify_or_regress (str): Enrichment, either 'classify' or 'regress' depending on whether external condition is categorical or continuous variable.
+    alpha_regularization (Union[float, List[float]]): Regularization parameter(s) for Ridge classifier.
+    verbose (bool): Whether to print detailed output. Default is False.
+
+    Returns:
+    anndata.AnnData: Merged AnnData object containing enrichment scores (classification or regression output) for spatially partitioned test folds
+    """
+
     all_folds_test_anndatas = []
 
     if not isinstance(alpha_regularization, list):
@@ -114,7 +202,6 @@ def conditional_motif_enrichment(adata: Anndata,
             test_anndata = adata[adata.obs[f'train_test_fold_{kfold}'] == 1]
 
             ## balance after train/test split b/c of spatial partitioning to downsample majority class
-
             train_anndata = balance_anndata(train_anndata, 'classify_condition')
             
             ## Train classifier
@@ -137,7 +224,37 @@ def conditional_motif_enrichment(adata: Anndata,
             test_anndata.obs['predicted_condition'] = predicted_condition
             all_folds_test_anndatas.append(test_anndata)
     elif classify_or_regress == "regress":
-        print('to be implemented')
+            
+        ## Train regressor on spatially partitioned train/test folds
+        for kfold in range(1,5):
+
+            print(f"Running kfold {kfold}")
+            
+            ## spatial split
+            train_anndata = adata[adata.obs[f'train_test_fold_{kfold}'] == 0]
+            test_anndata = adata[adata.obs[f'train_test_fold_{kfold}'] == 1]
+
+            ## TODO: add some sort of balancing w respect to binned condition
+            
+            ## Train regressor
+            regressor = RidgeCV(alphas=alpha_regularization)
+            regressor.fit(train_anndata.obsm[feature_key], train_anndata.obs['regress_condition'])
+
+            if verbose:
+                print("Fit alpha:")
+                print(regressor.alpha_)
+
+                ## Confirm train/test scores are similar to ensure regularization worked as expected
+                print("Train score:")
+                print(regressor.score(train_anndata.obsm[feature_key], train_anndata.obs['regress_condition']))
+
+                print("Test score:")
+                print(regressor.score(test_anndata.obsm[feature_key], test_anndata.obs['regress_condition']))
+                
+            ## Get conditional motif enrichment from trained classifier and write to anndata
+            predicted_condition = regressor.predict(test_anndata.obsm[feature_key])
+            test_anndata.obs['predicted_condition'] = predicted_condition
+            all_folds_test_anndatas.append(test_anndata)
 
 
     ## Merge test folds together
